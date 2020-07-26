cc85245178c7e628b87af741b8d79cb69b3bb0d7
av1m <36456709+av1m@users.noreply.github.com>
2020-07-26 23:05:55 +0200

    init github

--- app/index.php
+++ app/index.php
@@ -1,18 +1,4 @@
 <?php
-/**
- * Footer
- *
- * Main footer file for the theme.
- * php version 7.2.10
- *
- * @category   Components
- * @package    WordPress
- * @subpackage Theme_Name_Here
- * @author     Your Name <yourname@example.com>
- * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
- * @link       https://yoursite.com
- * @since      1.0.0
- */
 
 header("Location: controller/list.php");
 exit;
---
+++ app/modal/routing.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `short_urls` (
+	`id` INT(245) NOT NULL AUTO_INCREMENT,
+	`long_url` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	`short_code` VARCHAR(255) CHARACTER SET utf16 COLLATE utf16_general_ci NOT NULL,
+	`hits` INT(255) NOT NULL DEFAULT '0',
+	`created` DATE NOT NULL DEFAULT CURDATE(),
+	UNIQUE KEY `code` (`short_code`),
+	PRIMARY KEY (`id`,`short_code`)
+);
